6524ec110192f808157b7bef7c76ce998d1d24c9
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-08-24 13:47:43 +0900

    [Silver III] Title: N과 M (5), Time: 176 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,11 +1,13 @@
 def dfs(start, ans):
     if len(ans) == M:
-        print(*ans)
+        print(' '.join(map(str, ans))+'\n')
         return
     for i in range(0, N):
         if not arr[i] in ans:
             dfs(i+1, ans+[arr[i]])
 
+import sys
+print = sys.stdout.write
 N, M = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
 
