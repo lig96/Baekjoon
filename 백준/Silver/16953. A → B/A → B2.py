2d4234ac2ac7efe8c5c9094e27cc2c4fce2d80e2
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-08-27 17:01:38 +0900

    Update A → B2.py

--- 백준/Silver/16953. A → B/A → B2.py
+++ 백준/Silver/16953. A → B/A → B2.py
@@ -2,35 +2,25 @@
 
 from collections import deque
 A, B = map(int, input().split())
-ans = 1
 
 
-def bfs(graph, v):
-    global ans
-    global B
-
+def bfs(A, B):
     qu = deque()
-    qu.append([])
-    qu[-1].append(v)
+    qu.append((A, 1))
 
     while qu:
-        arr = qu.popleft()
+        i, level = qu.popleft()
 
-        ans += 1
-        qu.append([])
-        for i in arr:
-            for temp in [i*2, i*10+1]:
-                if temp == B:
-                    return
-                elif temp > B:
-                    pass
-                elif temp < B:
-                    qu[-1].append(temp)
-        if qu[-1] == []:
-            qu.pop()
-            ans = -1
+        for temp in [i*2, i*10+1]:
+            if temp == B:
+                print(level+1)
+                return
+            elif temp > B:
+                pass
+            elif temp < B:
+                qu.append((temp, level+1))
+    else:
+        print(-1)
 
 
-bfs(None, A)
-
-print(ans)
+bfs(A, B)
