9436adb48a79bffcf533375c99f6532af4febc7d
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-08-27 19:09:55 +0900

    Update RGB거리.py

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,12 +4,11 @@ input = sys.stdin.readline
 
 N = int(input())
 mat = []
-mat.append(list(map(int, input().split())))
+for i in range(N):
+    mat.append(list(map(int, input().split())))
 
 
 for i in range(1, N):
-    mat.append(list(map(int, input().split())))
-
     a, b, c = mat[i-1]
     mat[i][0] += min(b, c)
     mat[i][1] += min(a, c)
