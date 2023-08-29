da36fcd874f3ccce8f908e5d416632071efbb685
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-08-29 09:01:13 +0900

    [Silver I] Title: 절댓값 힙, Time: 132 ms, Memory: 35860 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,20 +1,40 @@
+
 from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
 
-heap = []
+plus = []
+minus = []
+
+
 N = int(input())
 for _ in range(N):
     x = int(input())
     if x != 0:
-        x = x+0.25 if x < 0 else x
-        sign = 1 if x > 0 else -1
-        heappush(heap, (abs(x), sign))
+        if x > 0:
+            heappush(plus, x)
+        else:
+            heappush(minus, -x)
     else:
-        if heap:
-            a, b = heappop(heap)
-            print(str(round(a)*b)+'\n')
+        if plus and minus:
+            num_plus = heappop(plus)
+            num_minus = -heappop(minus)
+            if abs(num_plus) < abs(num_minus):
+                print(str(num_plus)+'\n')
+                heappush(minus, -num_minus)
+            elif abs(num_plus) > abs(num_minus):
+                print(str(num_minus)+'\n')
+                heappush(plus, num_plus)
+            elif abs(num_plus) == abs(num_minus):
+                print(str(num_minus)+'\n')
+                heappush(plus, num_plus)
+        elif plus and (not minus):
+            num_plus = heappop(plus)
+            print(str(num_plus)+'\n')
+        elif (not plus) and minus:
+            num_minus = -heappop(minus)
+            print(str(num_minus)+'\n')
         else:
             print(str(0)+'\n')
