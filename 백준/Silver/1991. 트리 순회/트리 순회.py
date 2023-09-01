976e6d4308729103cb62838a27628b7b740eb05a
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-01 16:56:54 +0900

    [Silver I] Title: 트리 순회, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,40 @@
+
+left = dict()
+right = dict()
+for char in range(N := int(input())):
+    node, leftnode, rightnode = input().split()
+    left[node] = leftnode
+    right[node] = rightnode
+
+
+def preorder(start):
+    print(start, end='')
+    if left[start].isalpha():
+        preorder(left[start])
+    if right[start].isalpha():
+        preorder(right[start])
+
+
+def inorder(start):
+    if left[start].isalpha():
+        inorder(left[start])
+        print(start, end='')
+    else:
+        print(start, end='')
+    if right[start].isalpha():
+        inorder(right[start])
+
+
+def postorder(start):
+    if left[start].isalpha():
+        postorder(left[start])
+    if right[start].isalpha():
+        postorder(right[start])
+    print(start, end='')
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
