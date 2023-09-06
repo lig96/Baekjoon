de41d0a0eb4e478633cc62d0e0b541f8df02444a
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-06 16:52:21 +0900

    [Gold V] Title: 토마토, Time: 3940 ms, Memory: 50448 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+M, N, H = map(int, input().split())
+mat = [[list(map(int, input().split())) for _ in range(N)]
+       for _ in range(H)]
+# mat[H, height][N, row][M, col]
+cnt = 0
+qu = deque()
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if mat[h][n][m] == 0:
+                cnt += 1
+            elif mat[h][n][m] == 1:
+                qu.append((h, n, m, 0))
+dh = [0, 0, 1, -1, 0, 0]
+dn = [1, -1, 0, 0, 0, 0]
+dm = [0, 0, 0, 0, 1, -1]
+
+
+while qu:
+    h, n, m, day = qu.popleft()
+    for i in range(6):
+        nh, nn, nm = h+dh[i], n+dn[i], m+dm[i]
+        if (0 <= nh < H) and (0 <= nn < N) and (0 <= nm < M):
+            if mat[nh][nn][nm] == 0:
+                mat[nh][nn][nm] = 1
+                qu.append((nh, nn, nm, day+1))
+                cnt -= 1
+
+
+print(day) if cnt == 0 else print(-1)
