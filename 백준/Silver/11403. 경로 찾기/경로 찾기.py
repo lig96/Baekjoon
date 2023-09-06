c01e8e6e2e1ea43224a6b9d59b8e27e4f5e208a6
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-06 15:07:29 +0900

    [Silver I] Title: 경로 찾기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,24 +1,15 @@
 N = int(input())
-graph = []
-INF = int(1e9)
-for i in range(N):
-    temp = list(map(lambda x: INF if x == '0' else 1, input().split()))
-    graph.append(temp)
+graph = [list(map(int, input().split())) for _ in range(N)]
 
 
-for k in range(N):
-    for a in range(N):
-        for b in range(N):
-            old = graph[a][b]
-            new = graph[a][k] + graph[k][b]
-            if new < old:
-                graph[a][b] = new
+def dfs(i, visited):
+    for index, value in enumerate(graph[i]):
+        if (value == 1) and (not visited[index]):
+            visited[index] = 1
+            dfs(index, visited)
 
 
 for i in range(N):
-    for j in range(N):
-        if graph[i][j] < INF:
-            print(1, end=' ')
-        else:
-            print(0, end=' ')
-    print()
+    visited = [0 for _ in range(N)]
+    dfs(i, visited)
+    print(*visited)
