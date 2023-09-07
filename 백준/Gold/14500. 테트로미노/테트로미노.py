391f5f43a356f93ba6664e4f32363c11351f572d
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-07 20:01:16 +0900

    [Gold IV] Title: 테트로미노, Time: 6824 ms, Memory: 38004 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+ROW, COL = map(int, input().split())
+mat = [list(map(int, input().split())) for _ in range(ROW)]
+
+
+shapes = [
+    [(0, 0), (0, 1), (0, 2), (0, 3)],  # 청록
+    [(0, 0), (0, 1), (1, 0), (1, 1)],  # 노랑
+    [(0, 0), (1, 0), (2, 0), (2, 1)],  # 주황
+    [(0, 0), (1, 0), (1, 1), (2, 1)],  # 초록
+    [(0, 0), (0, 1), (0, 2), (1, 1)],  # 보라
+]
+
+
+ans = -1
+for _1 in range(4):
+    # 오른쪽으로 90도 회전
+    mat = [list(rows[::-1]) for rows in zip(*mat)]
+    for _2 in range(2):
+        # 위아래 대칭
+        mat = mat[::-1]
+        for r in range(len(mat)):
+            # 회전 시키는 과정에서 row의 최댓값이
+            # ROW, COL 사이로 변화함.
+            for c in range(len(mat[0])):
+                for shape in shapes:
+                    try:
+                        temp = sum([mat[r+dr][c+dc] for dr, dc in shape])
+                        ans = max(ans, temp)
+                    except:
+                        pass
+print(ans)
