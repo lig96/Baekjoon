3b1819c5dc5f6d8d234b501166cf547c6191982d
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-09 00:19:27 +0900

    Update 최소비용 구하기.py

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -5,11 +5,15 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
+# 반복문
 mat = [[] for _ in range(N+1)]
 for _ in range(M):
-    start, end, cost = map(int, input().split())
-    mat[start].append((end, cost))
-
+    start, end, c = map(int, input().split())
+    mat[start].append((end, c))
+# 리스트 축약
+# mat = [[] for _ in range(N+1)]
+# [mat[start].append((end, c)) for start, end, c in
+#  (map(int, input().split()) for _ in range(M))]
 ans_start, ans_end = map(int, input().split())
 
 
@@ -19,13 +23,13 @@ def dijk(start):
     heappush(heap, (0, start))
 
     while heap:
-        nowcost, now = heappop(heap)
-        if dist[now] < nowcost:
+        start_now, now_i = heappop(heap)
+        if dist[now_i] < start_now:
             continue
-        for end, endcost in mat[now]:
-            if nowcost+endcost < dist[end]:
-                dist[end] = dist[now]+endcost
-                heappush(heap, (dist[end], end))
+        for end_i, now_end in mat[now_i]:
+            if start_now+now_end < dist[end_i]:
+                dist[end_i] = start_now+now_end
+                heappush(heap, (dist[end_i], end_i))
 
 
 dist = [int(1e9) for _ in range(N+1)]
