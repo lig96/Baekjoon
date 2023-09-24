9a165b04c05cc8147ccdc66bc8331db14a2373ff
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-09-25 08:51:57 +0900

    Update 파이프 옮기기 1(DP).py

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1(DP).py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1(DP).py
@@ -9,25 +9,25 @@ for _ in range(N):
     mat.append(temp)
 
 
-dp = [[[0 for dir in range(3)]
-       for col in range(N+1)] for row in range(N+1)]
-# 인덱스를 1부터 맞춰주기 위해 1씩 추가.
-
+dp = [[[0 for d in range(3)] for r in range(N+1)] for c in range(N+1)]
+# 인덱스를 1부터 시작하기 위해 변형
+# dp[row][col][direction] 꼴
 
 dp[1][2][0] = 1
+# 초기값 1
 for r in range(1, N+1):
     for c in range(1, N+1):
         if r == 1 and c == 2:
-            # 초기값 1이 덮어씌워지는 것 방지
             continue
+            # 초기값 1이 덮어씌워지는 것 방지
         if mat[r][c] == 0:
             dp[r][c][0] = dp[r][c-1][0] + dp[r][c-1][2]
+            # 가로 파이프 = 가로->가로 + 대각선->가로
             dp[r][c][1] = dp[r-1][c][1] + dp[r-1][c][2]
+            # 세로 파이프
         if mat[r][c] == mat[r-1][c] == mat[r][c-1] == 0:
             dp[r][c][2] = dp[r-1][c-1][0] + dp[r-1][c-1][1] + dp[r-1][c-1][2]
+            # 대각선 파이프
 
 
 print(sum(dp[-1][-1]))
-# 0 가로
-# 1 세로
-# 2 대각선
