713396b170179f3642c27f8ec35ba6400d97f516
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-13 23:52:08 +0900

    Update 최단경로.py

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,4 +1,3 @@
-
 import sys
 from heapq import heappush, heappop
 input = sys.stdin.readline
@@ -14,29 +13,22 @@ def dijk(start):
         if dist_arr[now] < now_dist:
             continue
 
-        # for afters in graph[now].items():
-        for afters in graph[now]:
-            after, after_dist = afters
-
-            temp_dist = dist_arr[now] + after_dist
-            if dist_arr[after] > temp_dist:
-                dist_arr[after] = temp_dist
-                heappush(heap, (temp_dist, after))
-    return dist_arr
+        for after, after_dist in graph[now]:
+            temp = dist_arr[now] + after_dist
+            if dist_arr[after] > temp:  # 라인 A
+                dist_arr[after] = temp
+                heappush(heap, (temp, after))
 
 
 V, E = map(int, input().split())
 K = int(input())
-# graph = [{} for _ in range(V+1)]
-graph = [[]for _ in range(V+1)]
+graph = [[] for _ in range(V+1)]
 # 인덱싱을 1부터 하기 위해 V+1
-# 여러 E를 최소 경로로 초기화하기 위해 딕셔너리
+# 중복 간선을 최소 경로로 초기화하기 위해 dict를 쓸 수 있지만
+# 오히려 느리다. 라인 A에서 어차피 크기 비교가 이루어지기 때문에
+# 인접 리스트를 만드는 과정에서 크기 비교를 중복할 필요가 없는 듯하다.
 for _ in range(E):
     u, v, w = map(int, input().split())
-    # try:
-    #     graph[u][v] = min(graph[u][v], w)
-    # except:
-    #     graph[u][v] = w
     graph[u].append((v, w))
 
 
@@ -48,6 +40,6 @@ heap = []
 dijk(K)
 
 
-for i in range(1, V+1):
-    print(str(dist_arr[i]) if dist_arr[i] < INF else 'INF')
+for value in dist_arr[1:]:
+    print(str(value) if value < INF else 'INF')
     print('\n')
