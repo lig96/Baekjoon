8c8aef9416f8ccb8032c3ba5b421c560dee6492d
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-14 17:53:15 +0900

    [Gold IV] Title: 알파벳, Time: 5944 ms, Memory: 170236 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,30 +1,42 @@
-# https://campkim.tistory.com/18
+
+import sys
+input = sys.stdin.readline
 
 
-import sys 
-from collections import deque
-input=sys.stdin.readline
-R,C = map(int, input().split())
-arr=[list(input()) for _ in range(R)]
-check=[0]*(26)
+def dfs(graph, v, already, detph=1):
+    global ans
+    # visited랑 알파벳의 already랑 겹치기 때문에 필요 없음
 
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
-maxi=0
-
-def dfs(x,y,cnt):
-    global maxi
-    maxi=max(cnt,maxi)
+    r, c = v
+    already[ord(graph[r][c])] = True
     for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        if nx<R and ny<C and nx>=0 and ny>=0 and check[ord(arr[nx][ny])-65]==0:
-            check[ord(arr[nx][ny])-65]=1
-            ncnt=cnt+1
-            dfs(nx,ny,ncnt)
-            check[ord(arr[nx][ny])-65]=0
+        newr, newc = r+dr[i], c+dc[i]
+        if 0 <= newr < R and 0 <= newc < C and not already[ord(graph[newr][newc])]:
+            dfs(graph, (newr, newc), already, detph+1)
+    already[ord(graph[r][c])] = False
 
-check[ord(arr[0][0])-65]=1
-dfs(0,0,1)
+    # if sum(already)+1 == 26:
+    #     print(26)
+    #     exit()
+    # else:
+    ans = max(ans, detph)
 
-print(maxi)
+
+R, C = map(int, input().split())
+graph = [list(input()) for _ in range(R)]
+
+
+ans = 0
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
+
+dfs(graph, (0, 0), [False for _ in range(90+1)])
+
+# for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+#     print(ord(i))
+#     # 65 포함 ~ 90 포함
+
+print(ans)
+# 재귀를 끝낸 뒤 시작점을 버린 후 ans를 갱신하기 때문에
+# 1을 더해야 한다.
