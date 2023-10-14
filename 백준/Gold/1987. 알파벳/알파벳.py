116c9a332ca0c2ce8d027dbe595f59adeb21b359
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-14 19:45:54 +0900

    [Gold IV] Title: 알파벳, Time: 1316 ms, Memory: 53644 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,7 +1,3 @@
-# 기존에 거쳐온 알파벳들을 저장하기 위해
-# set와 list 모두 사용 가능한데 둘 다 O(1)이지만 list가 빠르다.
-# 시간 초과의 가장 간단한 해결책은 pypy로 제출하는 것이었다.
-
 import sys
 input = sys.stdin.readline
 
@@ -11,28 +7,22 @@ def dfs(graph, v, already, depth):
     # visited랑 알파벳의 already랑 겹치기 때문에 필요 없음
 
     r, c = v
-    already[ord(graph[r][c])] = True
-    for i in range(4):
-        newr, newc = r+dr[i], c+dc[i]
-        if 0 <= newr < R and 0 <= newc < C and not already[ord(graph[newr][newc])]:
-            dfs(graph, (newr, newc), already, depth+1)
-    already[ord(graph[r][c])] = False
+    stack = set()
+    stack.add((r, c, already+graph[r][c], depth))
 
-    # if depth == biggest:
-    #     print(depth)
-    #     exit()
-    # else:
-    #     ans = max(ans, depth)
-    # 조기 종료로 절약되는 시간보다
-    # 크기 비교 때문에 소모되는 시간이 긴 듯하다.
+    while stack:
+        nowr, nowc, nowalready, nowdepth = stack.pop()
+        ans = max(ans, nowdepth)
 
-    ans = max(ans, depth)
+        for i in range(4):
+            newr, newc = nowr+dr[i], nowc+dc[i]
+            if 0 <= newr < R and 0 <= newc < C and graph[newr][newc] not in nowalready:
+                stack.add((newr, newc, nowalready + graph[newr][newc], nowdepth+1))
     return
 
 
 R, C = map(int, input().split())
 graph = [list(input().strip()) for _ in range(R)]
-# biggest = len(set([x for y in graph for x in y]))
 
 
 ans = 0
@@ -40,7 +30,7 @@ dr = [0, 0, 1, -1]
 dc = [1, -1, 0, 0]
 
 
-dfs(graph, (0, 0), [False for _ in range(90+1)], 1)
+dfs(graph, (0, 0), '', 1)
 
 
 print(ans)
