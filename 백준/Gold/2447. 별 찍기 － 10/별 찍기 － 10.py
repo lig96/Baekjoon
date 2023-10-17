6f3206ac3af040d923ab9649f8f8a6f0098fab84
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-17 16:04:04 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 1664 ms, Memory: 69716 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,27 @@
+N = int(input())
+
+graph = [['*' for _ in range(N)] for _ in range(N)]
+
+
+def rec(row, col, width):
+    for r in range(row+width, row+2*width):
+        for c in range(col+width, col+2*width):
+            graph[r][c] = ' '
+
+    if width == 1:
+        return
+    else:
+        for i in [0, 1, 2, 3, 5, 6, 7, 8]:
+            row_new = row + width * (i//3)
+            col_new = col + width * (i % 3)
+            rec(row_new, col_new, width//3)
+            # 012
+            # 345
+            # 678
+
+
+rec(0, 0, N//3)
+
+
+for i in graph:
+    print(*i, sep='')
