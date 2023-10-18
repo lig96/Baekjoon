44147f9a3b3d4ce125395ae4ef699f4084aa0159
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-18 18:47:37 +0900

    Update 행렬 제곱.py

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,7 +1,6 @@
-
-
-N, B = map(int, input().split())
-mat = [x for x in (list(map(int, input().split())) for _ in range(N))]
+# 라인 A에서 return mat으로 바로 재귀문 탈출하게 된다면
+# 모듈러 연산을 거치지 않아서 오답이 난다.
+# print문에서 수정을 하든 항등 행렬과 계산을 시키든 연산을 거쳐야 한다.
 
 
 def mat_calc(mat1, mat2):
@@ -15,9 +14,7 @@ def mat_calc(mat1, mat2):
 
 def rec(mat, power):
     if power == 1:
-        eye = [[1 if row == col else 0 for col in range(N)]
-               for row in range(N)]
-        return mat_calc(mat, eye)
+        return mat_calc(mat, eye)  # 라인 A
     if power % 2 == 0:
         temp = rec(mat, power//2)
         return mat_calc(temp, temp)
@@ -25,6 +22,14 @@ def rec(mat, power):
         # 2^101 = 2^100 * 2
         temp = rec(mat, power-1)
         return mat_calc(temp, mat)
+
+
+N, B = map(int, input().split())
+mat = [x for x in (list(map(int, input().split())) for _ in range(N))]
+
+
+eye = [[1 if row == col else 0 for col in range(N)]
+       for row in range(N)]
 
 
 ans = rec(mat, B)
