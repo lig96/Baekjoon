3e00fd9250fb3ee75e8fdb17d2fdcd681e0a5ddf
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-18 19:24:23 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 296 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,32 @@
+
+N = int(input())
+A = list(map(int, input().split()))
+
+dp_in = [1 for _ in range(N)]
+# dp[i] = A[i]가 마지막일 때 증가 조건을 만족하는 수열의 최대 길이
+dp_de = [1 for _ in range(N)]
+# dp[i] = A[i]가 처음일 때 감소 조건을 만족하는 수열의 최대 길이
+
+
+for i in range(N):
+    for prev in range(i):
+        if A[i] > A[prev]:
+            temp = dp_in[prev]+1
+            if temp > dp_in[i]:
+                dp_in[i] = temp
+            # dp_in[i] = max(dp_in[i],
+        if A[-i-1] > A[-prev-1]:
+            temp = dp_de[-prev-1]+1
+            if temp > dp_de[-i-1]:
+                dp_de[-i-1] = temp
+            # dp_de[-i-1] = max(dp_de[-i-1], )
+
+
+ans = 0
+for i in range(N):
+    temp = dp_in[i]+dp_de[i]
+    if temp > ans:
+        ans = temp
+print(ans-1)
+
+# print(max([dp_in[i]+dp_de[i] for i in range(N)])-1)
