658f41d56372374882f49132273bf88789d16f58
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-19 01:20:01 +0900

    Update 점프 숨바꼭질.py

--- 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.py
+++ 백준/Silver/22973. 점프 숨바꼭질/점프 숨바꼭질.py
@@ -1,22 +1,30 @@
+####################
+# https://leeingyun96.tistory.com/23
 
-K = abs(int(input()))
+####################
+# 1.
 
+K = int(input())
 if K == 0:
     print(0)
-    exit()
 elif K % 2 == 0:
     print(-1)
-    exit()
+else:
+    print(K.bit_length())
 
+####################
+# 2.
 
-ans = 0
-while K > 0:
-    if (K & 1):
+K = abs(int(input()))
+if K == 0:
+    print(0)
+elif K % 2 == 0:
+    print(-1)
+else:
+    ans = 0
+    while K > 0:
         K = K >> 1
         ans += 1
-        # print(K)
-    else:
-        ans += 1
-        K = K >> 1
-        # print(K)
-print(ans)
+    print(ans)
+
+####################
