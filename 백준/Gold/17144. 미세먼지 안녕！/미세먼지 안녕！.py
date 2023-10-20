6b974e2618cc190e5161c42aa8c5e22cfc7da672
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-10-21 03:32:11 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 2984 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -2,85 +2,60 @@ import sys
 input = sys.stdin.readline
 
 
-R, C, T = map(int, input().split())
-graph = [x for x in
-         (list(map(int, input().split())) for _ in range(R))]
-
-# print(*graph, sep='\n')
-
-
-dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
-
-
 def spread():
     new_graph = [[0 for _ in range(C)] for _ in range(R)]
 
     for r in range(R):
         for c in range(C):
-            if graph[r][c] == 0:
+            new_graph[r][c] += graph[r][c]
+            if graph[r][c] <= 0:
                 continue
-            elif graph[r][c] == -1:
-                new_graph[r][c] = -1
-                continue
-
-            can_dirs = 0
+            amount = graph[r][c]//5
             for i in range(4):
                 nr, nc = r+dr[i], c+dc[i]
                 if 0 <= nr < R and 0 <= nc < C and graph[nr][nc] != -1:
-                    can_dirs += 1
-            spread_amount = graph[r][c] // 5
-            remain_amount = graph[r][c] - can_dirs*spread_amount
-
-            new_graph[r][c] += remain_amount
-            for i in range(4):
-                nr, nc = r+dr[i], c+dc[i]
-                if 0 <= nr < R and 0 <= nc < C and graph[nr][nc] != -1:
-                    new_graph[nr][nc] += spread_amount
+                    new_graph[nr][nc] += amount
+                    new_graph[r][c] -= amount
     return new_graph
 
 
-def clean():
-    flag_next_cleaner = 'upper'
-    for r in range(R):
-        for c in range(C):
-            if not (graph[r][c] == -1):
-                continue
-            if flag_next_cleaner == 'upper':
-                before = 0
-                for nc in range(1, C):
-                    graph[r][nc], before = before, graph[r][nc]
-                    # 오른쪽
-                for nr in range(r-1, -1, -1):
-                    graph[nr][-1], before = before, graph[nr][-1]
-                    # 위쪽
-                for nc in range(C-1-1, -1, -1):
-                    graph[0][nc], before = before, graph[0][nc]
-                    # 왼쪽
-                for nr in range(1, r):
-                    graph[nr][0], before = before, graph[nr][0]
-                    # 아래쪽
-                flag_next_cleaner = 'lower'
-            elif flag_next_cleaner == 'lower':
-                before = 0
-                for nc in range(1, C):
-                    graph[r][nc], before = before, graph[r][nc]
-                    # 오른쪽
-                for nr in range(r+1, R):
-                    graph[nr][-1], before = before, graph[nr][-1]
-                    # 아래쪽
-                for nc in range(C-1-1, -1, -1):
-                    graph[-1][nc], before = before, graph[-1][nc]
-                    # 왼쪽
-                for nr in range(R-1-1, r, -1):
-                    graph[nr][0], before = before, graph[nr][0]
-                    # 위쪽
-                flag_next_cleaner = 'no next clear'
-    return graph
+def clean(type, rc):
+    r, c = rc
+    if type == 'upper':
+        dr = [0, -1, 0, 1]
+        dc = [1, 0, -1, 0]
+        # 오른쪽, 위쪽, 왼쪽, 아래쪽
+    elif type == 'lower':
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+        # 오른쪽, 아래쪽, 왼쪽, 위쪽
+
+    before = 0
+    for i in range(4):
+        while True:
+            nr, nc = r+dr[i], c+dc[i]
+            if 0 <= nr < R and 0 <= nc < C and graph[nr][nc] != -1:
+                # 일직선으로 쭉 가는 것의 탈출 조건은
+                # 벽에 부딪히거나, 한 바퀴 돌고 에어컨에 부딪힐 때
+                graph[nr][nc], before = before, graph[nr][nc]
+                r, c = nr, nc
+            else:
+                break
+
+
+R, C, T = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(R)]
+
+
+dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+cleaners = [(r, 0) for r in range(R) if graph[r][0] == -1]
 
 
 for _ in range(T):
     graph = spread()
-    clean()
+    clean('upper', cleaners[0])
+    clean('lower', cleaners[1])
 
-ans = sum([sum(x) for x in graph])+2
+
+ans = sum(map(sum, graph)) + 2
 print(ans)
