f515fd458ccc7d4bd0b89b8efbd92a3d2b425e2c
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 07:34:44 +0900

    [Gold V] Title: Contact, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.py
+++ 백준/Gold/1013. Contact/Contact.py
@@ -1,22 +1,36 @@
-# 반복 사용할 땐 p = re.compile()
-# search - ~
-# match - 시작~
-# fullmatch - 시작~끝
-# ^, $를 통해 동일 작동 가능.
 
 
-import re
 import sys
 input = sys.stdin.readline
 
 
-pattern = re.compile('(100+1+|01)+')
+def sol(s):
+    st = 0
+    states = {}
+    # 다음state = states[현재state][입력]
+    states[0] = [1, 2]  # default 혹은 right01이 끝난 후
+    states[1] = [99, 0]  # right, 0
+    states[2] = [3, 99]  # left, 1
+    states[3] = [4, 99]  # left, 10
+    states[4] = [4, 5]  # left, 100+
+    states[5] = [1, 6]  # left, 100+1
+    states[6] = [7, 6]  # left, 100+11
+    states[7] = [4, 0]  # left, 100+110
+    states[99] = [99, 99]
+
+    for char in s:
+        st = states[st][int(char)]
+        # print(st, char)
+    return st
 
 
 T = int(input())
 for _ in range(T):
     s = input().rstrip()
 
-    arr = pattern.fullmatch(s)
+    ans = sol(s)
 
-    print('YES' if arr else 'NO')
+    if ans in [0, 5, 6]:
+        print('YES')
+    else:
+        print('NO')
