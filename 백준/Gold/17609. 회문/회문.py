50952a82df7004204fd4740aed7f45bd081ea859
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 06:03:36 +0900

    [Gold V] Title: 회문, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -2,14 +2,18 @@ import sys
 input = sys.stdin.readline
 
 
-def sol(string, type):
+def sol(s, type):
     if type == 2:
         return
-    for i in range(0, ((len(string)+1)//2)):
-        j = len(string)-i-1
-        if string[i] != string[j]:
-            sol(string[:i]+string[i+1:], type+1)
-            sol(string[:j]+string[j+1:], type+1)
+    for i in range(0, ((len(s)+1)//2)):
+        j = len(s)-i-1
+        if s[i] != s[j]:
+            # s = 1s[:i]+2s[i]+3s[i+1:j]+4s[j]+5s[j+1:]
+            # 1, 5는 이미 회문인 게 확인이 되었으니 불필요.
+            # 2, 4는 서로 다름. 3은 모름.
+            # sol(start_i, end_i)처럼 투포인터로 풀이 가능.
+            sol(s[i:j], type+1)  # 23
+            sol(s[i+1:j+1], type+1)  # 34
             ans.append(2)
             return
     else:
@@ -19,9 +23,10 @@ def sol(string, type):
 
 T = int(input())
 for _ in range(T):
-    string = input().rstrip()
+    s = input().rstrip()
 
     ans = []
-    sol(string, 0)
+    sol(s, 0)
 
     print(min(ans))
+    # print(ans)
