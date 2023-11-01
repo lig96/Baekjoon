752c44bf198bd5d3e4b7cada768d679306d0567a
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 05:01:35 +0900

    Update A와 B.py

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,8 +1,7 @@
-
-# bfs로는 불가능
-# len(S)=1, len(T)=1000이라면 2^999를 탐색해야 함
+# bfs로는 불가능. len(S)=1, len(T)=1000이라면 2^999를 탐색해야 함.
 # 그리디로 접근해야 한다.
-
+# 문자열 혹은 리스트를 뒤집는 것은 O(N)이라서 deque를 사용했는데
+# O(1)의 상수가 커서인지 약간이나마 느리다.
 
 from collections import deque
 import sys
@@ -25,22 +24,24 @@ def sol():
             else:
                 string.popleft()
                 is_reversed = not is_reversed
-    if not is_reversed:
-        if ''.join(string) == S:
-            return 1
-        else:
-            return 0
     else:
-        if ''.join(string)[::-1] == S:
-            return 1
-
+        if not is_reversed:
+            if ''.join(string) == S:
+                return 1
+            else:
+                return 0
         else:
-            return 0
+            if ''.join(string)[::-1] == S:
+                return 1
+            else:
+                return 0
 
 
 S = input().rstrip()
 T = input().rstrip()
 
+
 ans = sol()
+
 
 print(ans)
