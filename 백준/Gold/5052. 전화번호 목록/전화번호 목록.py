f02f93df76d715c8352f628e890ec19b859d30d4
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 04:20:14 +0900

    Update 전화번호 목록.py

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,5 +1,4 @@
-
-# 공간복잡도 O(개수 * 2*길이)
+# 시간복잡도 O(개수 * 2*길이)
 
 
 import sys
@@ -7,12 +6,12 @@ input = sys.stdin.readline
 
 
 def sol(nums):
-    # for small, big in zip(nums[:-1], nums[1:]):
     for i in range(len(nums)-1):
         small, big = nums[i], nums[i+1]
         # str을 오름차순 정렬했기 때문에
         # 바로 옆자리인데 len이 크다면 접두사가 동일함
-        if (len(small) < len(big)) and big.startswith(small):
+        if (len(small) < len(big)) and (big[:len(small)] == small):
+            # big.startswith(small)
             return 'NO'
     return 'YES'
 
