67d3c3cdad63e40ae4f25018de5ea375eb8232a3
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 21:44:26 +0900

    Update 개미굴.py

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -1,3 +1,44 @@
+# # 이전 코드
+# import sys
+# input = sys.stdin.readline
+
+
+# graph = dict()
+
+
+# N = int(input())
+# for _ in range(N):
+#     temp = input().split()
+#     K, infos = temp[0], temp[1:]
+
+#     now, now_d, now_dict = 'default', 0, graph
+#     for info in infos:
+#         if (now, now_d) in now_dict.keys():
+#             if (info, now_d+1) not in now_dict[(now, now_d)].keys():
+#                 now_dict[(now, now_d)][(info, now_d+1)] = dict()
+#         else:
+#             now_dict[(now, now_d)] = dict()
+#             now_dict[(now, now_d)][(info, now_d+1)] = dict()
+#         now, now_d, now_dict = info, now_d+1, now_dict[(now, now_d)]
+
+
+# def rec(start, graph):
+#     if start not in graph.keys():
+#         return
+#     for now, now_d in sorted(graph[start].keys()):
+#         print('--'*(now_d-1), now, sep='')
+#         rec((now, now_d), graph[start])
+
+
+# rec(('default', 0), graph)
+
+
+
+
+# 이전 코드에서 trie를 참고해서 약간 수정한 코드
+# 코드에서 불필요한 부분이 너무 많았다.
+
+
 import sys
 input = sys.stdin.readline
 
@@ -12,21 +53,19 @@ for _ in range(N):
 
     now, now_d, now_dict = 'default', 0, graph
     for info in infos:
-        if (now, now_d) in now_dict.keys():
-            if (info, now_d+1) not in now_dict[(now, now_d)].keys():
-                now_dict[(now, now_d)][(info, now_d+1)] = dict()
-        else:
-            now_dict[(now, now_d)] = dict()
-            now_dict[(now, now_d)][(info, now_d+1)] = dict()
-        now, now_d, now_dict = info, now_d+1, now_dict[(now, now_d)]
+        # if (now, now_d) in now_dict.keys():
+        if (info) not in now_dict.keys():
+            now_dict[info] = dict()
+        # else:
+            # now_dict[(now, now_d)] = dict()
+            # now_dict[(now, now_d)][(info, now_d+1)] = dict()
+        now, now_d, now_dict = info, now_d+1, now_dict[info]
 
 
-def rec(start, graph):
-    if start not in graph.keys():
-        return
-    for now, now_d in sorted(graph[start].keys()):
-        print('--'*(now_d-1), now, sep='')
-        rec((now, now_d), graph[start])
+def rec(depth, graph):
+    for now in sorted(graph.keys()):
+        print('--'*(depth), now, sep='')
+        rec(depth+1, graph[now])
 
 
-rec(('default', 0), graph)
+rec(0, graph)
