144adf0f3ac333adce104aa8bfc6490837f5fe55
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 18:36:16 +0900

    Update 한글 LCS.py

--- 백준/Gold/15482. 한글 LCS/한글 LCS.py
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.py
@@ -1,17 +1,25 @@
-import sys
+# import sys
+# if 'utf-8' != sys.stdin.encoding:
+#     raise Exception
+# 로컬 환경은 다를 수 있지만
+# 백준 python3 환경은 utf-8
+# 백준 pypy3 환경은 utf-8이 아닌 무언가
+
 # print(sys.getdefaultencoding())
 # print(sys.stdin.encoding)
 # print(sys.stdout.encoding)
+# 인코딩을 확인하는 방법은 위와 같고
+
 # sys.stdin.reconfigure(encoding='utf-8')
 # sys.stdout.reconfigure(encoding='utf-8')
-# print(sys.getdefaultencoding())
-# print(sys.stdin.encoding)
-# print(sys.stdout.encoding)
+# 인코딩을 변경하는 방법은 위와 같다.
 
-if 'utf-8' != sys.stdin.encoding:
-    raise Exception
+# python, pypy 둘 다 전부 주석 처리 해도 답 처리 된다.
+# 출제자가 의도한 풀이는 유니코드를 3자리씩 끊어서 비교하기인 듯하다.
+# C와 달리 파이썬에서는 1글자로 처리되는 듯하다.
 
 a, b = input(), input()
+
 
 dp = [[0 for _ in range(len(b)+1)]
       for _ in range(len(a)+1)]
