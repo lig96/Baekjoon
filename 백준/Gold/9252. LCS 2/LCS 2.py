2e77cd8e22bc37a227b7fdcb0e83bdf05012eb5f
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-02 17:32:48 +0900

    [Gold IV] Title: LCS 2, Time: 148 ms, Memory: 118400 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,20 +1,30 @@
 a, b = input(), input()
 
 
-dp = [['' for _ in range(len(b)+1)]
+dp = [[0 for _ in range(len(b)+1)]
       for _ in range(len(a)+1)]
 
 
 for i in range(1, len(a)+1):
     for j in range(1, len(b)+1):
         if a[i-1] == b[j-1]:
-            dp[i][j] = dp[i-1][j-1] + a[i-1]
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
-            dp[i][j] = sorted([dp[i-1][j], dp[i][j-1]],
-                              key=lambda x: -len(x))[0]
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-if dp[-1][-1]:
-    print(len(dp[-1][-1]))
-    print(dp[-1][-1])
-else:
-    print(0)
+i, j = len(a), len(b)
+ans_arr = []
+while i != 0 and j != 0:
+    if a[i-1] == b[j-1]:
+        ans_arr.append(a[i-1])
+        i, j = i-1, j-1
+    elif dp[i][j] == dp[i-1][j]:
+        i = i-1
+    elif dp[i][j] == dp[i][j-1]:
+        j = j-1
+    else:
+        raise Exception
+    
+
+print(dp[-1][-1])
+print(''.join(ans_arr)[::-1])
