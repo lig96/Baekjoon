22a77b8e8f36efaa04d16a5306b1ac2e252782fd
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-04 01:28:29 +0900

    Update Список школ.py

--- 백준/Silver/21508. Список школ/Список школ.py
+++ 백준/Silver/21508. Список школ/Список школ.py
@@ -1,4 +1,18 @@
+import re
 from collections import defaultdict
+
+
+def make_num_by_regex():
+    num = re.search('[\d]+', school).group()
+    return num
+
+
+def make_num_by_loop():
+    num = ''
+    for char in school:
+        if char.isdecimal():
+            num += char
+    return num
 
 
 dic = defaultdict(lambda: 0)
@@ -7,10 +21,8 @@ dic = defaultdict(lambda: 0)
 n = int(input())
 for _ in range(n):
     school = input()
-    num = ''
-    for char in school:
-        if char.isdecimal():
-            num = num + char
+    # num = make_num_by_regex()
+    num = make_num_by_loop()
     dic[num] += 1
 
 ans = [k for k, v in dic.items() if v <= 5]
