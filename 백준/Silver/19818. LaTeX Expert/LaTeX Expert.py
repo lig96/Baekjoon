3e277543283984a5eed0a490026823e4538a5e19
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-04 05:55:45 +0900

    Update LaTeX Expert.py

--- 백준/Silver/19818. LaTeX Expert/LaTeX Expert.py
+++ 백준/Silver/19818. LaTeX Expert/LaTeX Expert.py
@@ -1,33 +1,40 @@
+# 'sys.stdin.readlines()에는 \n이 들어있고 맨 마지막 줄에는 \n이 없다'
+# 라고 알고 있고 실제로도 그러한데
+# 백준 채점 방식에서는 마지막 줄에도 \n이 붙는 듯하다.
+# 문제 자체는 간단했으나 디버깅하기 어려웠다.
+
 import sys
 import re
 
-lines = sys.stdin.readlines()
-begin = '\\begin{thebibliography}{99}\n'
-ind = lines.index(begin)
-text, bib = lines[:ind], lines[ind:]
 
-
-p = re.compile(r'\\bibitem\{(.+?)\}')
+is_text = True
+p = re.compile(r'\{(.+?)\}')
+now_order, correct_order = [], []
 bib_dic = {}
-for line in bib:
-    cites = p.findall(line)
-    for cite in cites:
-        bib_dic[cite] = line
-# dict[사람] : 사람. 사람. 년도.
-
-p = re.compile(r'\\cite\{(.+?)\}')
-ans_from_text = []
-ans_from_text.append(begin)
-for line in text:
-    cites = p.findall(line)
-    for cite in cites:
-        ans_from_text.append(bib_dic[cite])
-end = '\\end{thebibliography}\n'  # readlines() 맨 뒤에는 \n이 없음
-ans_from_text.append(end)
 
 
-if bib == ans_from_text:
+for line in sys.stdin.readlines():
+    line = line.rstrip()  # 라인 A
+    if line == '\\begin{thebibliography}{99}':
+        is_text = False
+
+    if is_text:
+        cites = p.findall(line)
+        for cite in cites:
+            correct_order.append(cite)
+    else:
+        cites = p.findall(line)
+        for cite in cites:
+            now_order.append(cite)
+            bib_dic[cite] = line
+
+
+# 입력된 bib에는 thebib, 99, ~~, thebib이 들어있음.
+if now_order[2:-1] == correct_order:
     print('Correct')
 else:
     print('Incorrect')
-    print(''.join(ans_from_text))
+    print('\\begin{thebibliography}{99}')
+    for i in correct_order:
+        sys.stdout.write(bib_dic[i]+'\n')
+    print('\\end{thebibliography}')
