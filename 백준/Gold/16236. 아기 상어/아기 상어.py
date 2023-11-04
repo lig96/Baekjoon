0e025c82b24d3e641adbb7fd1c1fc05dae0ef592
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-05 07:34:38 +0900

    [Gold III] Title: 아기 상어, Time: 164 ms, Memory: 114224 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,41 +1,42 @@
+# 깊이를 저장할 필요가 있다면 visited 배열을 이용하자.
+
+
 from collections import deque
 
 
 def bfs(graph, sr, sc):
-    global shark
-
     qu = deque()
-    qu.append((sr, sc, 0))
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    visited[sr][sc] = True
+    qu.append((sr, sc))
+    visited = [[-1 for _ in range(N)] for _ in range(N)]
+    visited[sr][sc] = 0
     at_least_one_fish = False
 
     while qu:
-        r, c, d = qu.popleft()
+        r, c = qu.popleft()
 
-        if at_least_one_fish and d > at_least_one_fish:
+        if at_least_one_fish and visited[r][c] > at_least_one_fish:
             # 깊이 3에서 찾았으면 깊이 4 물고기는 안 찾아도 됨
             break
 
         for i in range(4):
-            nr, nc, nd = r+dr[i], c+dc[i], d+1
-            if 0 <= nr < N and 0 <= nc < N and not visited[nr][nc]:
+            nr, nc = r+dr[i], c+dc[i]
+            if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == -1:
                 fish = graph[nr][nc]
                 if fish > shark:
                     pass
                 elif fish == shark:
-                    qu.append((nr, nc, nd))
-                    visited[nr][nc] = True
+                    qu.append((nr, nc))
+                    visited[nr][nc] = visited[r][c]+1
                 else:  # fish < shark
                     if fish == 0:  # 빈 공간
-                        qu.append((nr, nc, nd))
-                        visited[nr][nc] = True
+                        qu.append((nr, nc))
+                        visited[nr][nc] = visited[r][c]+1
                     else:  # fish 존재
-                        qu.append((nr, nc, nd))
-                        visited[nr][nc] = True
-                        foods.append((nr, nc, nd))
+                        qu.append((nr, nc))
+                        visited[nr][nc] = visited[r][c]+1
+                        foods.append((nr, nc, visited[nr][nc]))
                         if not at_least_one_fish:
-                            at_least_one_fish = nd
+                            at_least_one_fish = visited[nr][nc]
     return
 
 
@@ -43,7 +44,13 @@ N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
 
-[(sr, sc)] = [(r, c) for r in range(N) for c in range(N) if graph[r][c] == 9]
+for r in range(N):
+    for c in range(N):
+        if graph[r][c] == 9:
+            sr, sc = r, c
+            graph[r][c] = 0  # 9는 상어의 위치일 뿐이니 필요 없음
+
+
 dr, dc = [0, 0, 1, -1], [1, -1, 0, 0]
 shark, shark_stomach = 2, 0
 
@@ -52,9 +59,9 @@ ans = 0
 while True:
     foods = []
     bfs(graph, sr, sc)
+
     if foods:
         foods.sort(key=lambda x: (x[2], x[0], x[1]))  # depth, r, c 순서
-        graph[sr][sc] = 0  # 상어 이전 자리 초기화
         sr, sc, depth = foods[0]  # 상어 이후 자리 선언
         graph[sr][sc] = 0  # 물고기 이전 자리 초기화
         shark_stomach += 1
