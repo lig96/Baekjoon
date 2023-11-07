8f40746c08a72256deb21df5dab443571d75ed7a
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-07 10:40:08 +0900

    [Gold V] Title: 0 만들기, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,11 +1,11 @@
-# 1013
+import sys
+print = sys.stdout.write
 
 
 def dfs(n, string):
     if n == N+1:
-        string2 = ''.join([x for x in string if x != ' '])
-        if eval(string2) == 0:
-            print(string)
+        if eval(string.replace(' ', '')) == 0:
+            print(string+'\n')
             return
         else:
             return
@@ -14,6 +14,9 @@ def dfs(n, string):
         dfs(n+1, str(n))
     else:
         for oper in [' ', '+',  '-']:
+            # # ASCII 순서에 따라
+            # print(oper, ord(oper))
+            # 32, 43, 45
             dfs(n+1, string+oper+str(n))
 
 
@@ -21,4 +24,4 @@ T = int(input())
 for _ in range(T):
     N = int(input())
     dfs(1, '')
-    print()
+    print('\n')
