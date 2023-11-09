b030b17d5a497088d115fcf4c4575e776e92a964
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-09 19:41:50 +0900

    Update XML.py

--- 백준/Platinum/4828. XML/XML.py
+++ 백준/Platinum/4828. XML/XML.py
@@ -11,34 +11,40 @@ def sol(s):
     #     return 'invalid'
 
     # 1. <> 검사.
-    # 올바른 <, 틀린 <
-    # 올바른 >, 틀린 >
-    # 올바른 <tag>
-    # 개수가 동일하다는 것은 틀린 <, 틀린 >이 없다는 뜻
-    temp = re.findall('<', s)
-    temp2 = re.findall('>', s)
-    temp3 = re.findall('(<[0-9a-z]+>)|(</[0-9a-z]+>)|(<[0-9a-z]+/>)', s)
-    if not (len(temp) == len(temp2) == len(temp3)):
-        return 'invalid'
+    if 'method 1':
+        temp = re.findall('<', s)  # 올바른 <, 틀린 <
+        temp2 = re.findall('>', s)  # 올바른 >, 틀린 >
+        temp3 = re.findall(
+            '(<[0-9a-z]+>)|(</[0-9a-z]+>)|(<[0-9a-z]+/>)',
+            s)  # 올바른 <tag>
+        if not (len(temp) == len(temp2) == len(temp3)):
+            return 'invalid'
+    elif 'method 2':
+        temp = re.findall(
+            '(<[0-9a-z]+>)|(</[0-9a-z]+>)|(<[0-9a-z]+/>)|([<>])',
+            s)  # 틀린 <, >
+        temp = [x[3] for x in temp if x[3]]
+        if temp:
+            return 'invalid'
 
     # 23. 문자열 검사
     # 대체가 아니라 제거해야 함. 반례 &lt;tag&gt;
     s = re.sub('&lt;|&gt;|&amp;', '', s)
 
     # 1. & 검사. 4. HEX 검사.
-    # # 방법 1
-    # & 뒤 올바른 HEX
-    # & 뒤 올바른 HEX, 틀린 HEX, HEX가 아닌 문자열
-    # 개수가 동일하다는 것은 틀린 HEX, HEX가 아닌 문자열이 없다는 뜻
-    # temp = re.findall('&x([0-9A-Fa-f][0-9A-Fa-f])+;', s)
-    # temp2 = re.findall('&', s)
-    # if len(temp) != len(temp2):
-    #     return 'invalid'
-    # # 방법 2
-    # & 뒤 올바른 HEX가 오지 않는 경우
-    temp = re.findall('&(?!x([0-9A-Fa-f][0-9A-Fa-f])+;)', s)
-    if temp:
-        return 'invalid'
+    if 'method 1':
+        temp = re.findall(
+            '&x([0-9A-Fa-f][0-9A-Fa-f])+;',
+            s)  # & 뒤 올바른 HEX
+        temp2 = re.findall('&', s)  # &
+        if len(temp) != len(temp2):
+            return 'invalid'
+    elif 'method 2':
+        temp = re.findall(
+            '&(?!x([0-9A-Fa-f][0-9A-Fa-f])+;)',
+            s)  # & 뒤 올바른 HEX가 오지 않는 경우
+        if temp:
+            return 'invalid'
 
     # 567. 스택 검사.
     stack = []
