fb30a6ba36f22770f1097ced5c8e0c5619b7e12d
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-11-27 21:21:49 +0900

    Update CCW.py

--- 백준/Gold/11758. CCW/CCW.py
+++ 백준/Gold/11758. CCW/CCW.py
@@ -1,14 +1,21 @@
 # CCW
 # 두 점을 지나는 직선의 방정식
+# 신발끈
 
 
 def sol(p1, p2, p3):
     x1, y1 = p1
     x2, y2 = p2
     x3, y3 = p3
+    # x1, y1 추가
 
-    left = (y3-y1)*(x2-x1)
-    right = (y2-y1)*(x3-x1)
+    # 두 점을 지나는 직선의 방정식
+    # left = (y3-y1)*(x2-x1)
+    # right = (y2-y1)*(x3-x1)
+    # 신발끈 공식
+    left = (x1*y2)+(x2*y3)+(x3*y1)
+    right = (y1*x2)+(y2*x3)+(y3*x1)
+
     if left == right:
         return 0
     elif left > right:
