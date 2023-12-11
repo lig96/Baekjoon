7c1684fb525afae1fb8c5b94dc6c2967f19bb1ce
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-12-11 19:42:23 +0900

    Update 가장 긴 증가하는 부분 수열 5.py

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -4,49 +4,19 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 
-def sol():
-    for i in range(0, 1):
-        v = arr[i]
-        values.append(v)
-
-        indexes.append(i)
-        parent[i] = 'first'
-    for i in range(1, N):
-        v = arr[i]
-        if values[-1] < v:
-            values.append(v)
-
-            indexes.append(i)
-            parent[i] = indexes[-1-1]
-        else:
-            ind = bisect_left(values, v)
-            values[ind] = v
-
-            indexes[ind] = i
-            if ind == 0:
-                parent[i] = 'first'
-            else:
-                parent[i] = indexes[ind-1]
-    return
-
-
 def print_ans():
-    print(str(len(values))+'\n')
+    ans = []
+    ans_ind = indexes[-1]
+    while True:
+        ans.append(arr[ans_ind])
+        if parent[ans_ind] == 'first':
+            break
+        else:
+            ans_ind = parent[ans_ind]
 
-    if len(values) == N:
-        for v in arr:
-            print(str(v)+' ')
-    else:
-        ans = []
-        ans_ind = indexes[-1]
-        while True:
-            ans.append(arr[ans_ind])
-            if parent[ans_ind] == 'first':
-                break
-            else:
-                ans_ind = parent[ans_ind]
-        for v in ans[::-1]:
-            print(str(v)+' ')
+    print(str(len(ans))+'\n')
+    for v in ans[::-1]:
+        print(str(v)+' ')
     return
 
 
@@ -54,12 +24,33 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 
-parent = ['default' for _ in range(N)]
+parent = [-1 for _ in range(N)]
 values = []
 indexes = []
 
 
-sol()
+for i in range(0, 1):
+    v = arr[i]
+    values.append(v)
+
+    indexes.append(i)
+    parent[i] = 'first'
+for i in range(1, N):
+    v = arr[i]
+    if values[-1] < v:
+        values.append(v)
+
+        indexes.append(i)
+        parent[i] = indexes[-1-1]
+    else:
+        ind = bisect_left(values, v)
+        values[ind] = v
+
+        indexes[ind] = i
+        if ind == 0:
+            parent[i] = 'first'
+        else:
+            parent[i] = indexes[ind-1]
 
 
 print_ans()
