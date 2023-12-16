ac529c96ca3c72194a268a474f65104edb0d99aa
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-12-16 23:53:43 +0900

    Update LIST.py

--- 백준/Gold/3291. LIST/LIST.py
+++ 백준/Gold/3291. LIST/LIST.py
@@ -4,20 +4,24 @@ arr = [(input(), input()) for _ in range(N)]
 
 ans_arr = ['' for _ in range(N)]
 for i, (name, oper) in enumerate(arr):
+    # same이면 그대로 넣기
     if oper == 'SAME':
         ans_arr[i] = name
 for i, (name, oper) in enumerate(arr):
+    # down이면 정방향으로 순회하며 맨 왼쪽에 넣기
     if oper == 'DOWN':
         for j in range(0, i):
             if not ans_arr[j]:
                 ans_arr[j] = name
                 break
-for i, (name, oper) in enumerate(arr):
+for i, (name, oper) in list(enumerate(arr))[::-1]:
+    # up이면 역방향으로 순회하며 맨 오른쪽에 넣기
     if oper == 'UP':
-        for j in range(i+1, N):
+        for j in range(i+1, N)[::-1]:
             if not ans_arr[j]:
                 ans_arr[j] = name
                 break
+# same이 맨 앞이라면 down과 up은 순서 상관 없음
 
 
 for ans in ans_arr:
