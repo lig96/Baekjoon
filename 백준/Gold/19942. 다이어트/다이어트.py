d2671f207de08dc0c7892f0fceb0abfeb3b15cad
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-12-18 19:02:09 +0900

    [Gold IV] Title: 다이어트, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.py
+++ 백준/Gold/19942. 다이어트/다이어트.py
@@ -1,3 +1,8 @@
+# 명시적으로 ans_arr의 대소 조건을 구현하였다.
+# '넣는다', '안 넣는다'의 순서에 따라 암묵적으로 구현하면
+# 다양한 경우에서 오류가 난다.
+
+
 def dfs(depth, pfsvc, temp_ans_arr):
     global ans, ans_arr
 
@@ -5,10 +10,11 @@ def dfs(depth, pfsvc, temp_ans_arr):
         if not is_healthy(pfsvc):
             return
         if ans > pfsvc[4]:
+            # ans가 갱신된다면 무조건 ans_arr가 갱신됨
             ans = pfsvc[4]
             ans_arr = temp_ans_arr[::]
-        elif ans == pfsvc[4]:
-            # ans = pfsvc[4]
+        elif ans == pfsvc[4]:  # 라인 A
+            # ans가 동일하다면 특정 조건에서 ans_arr가 갱신됨
             if temp_ans_arr < ans_arr:
                 ans_arr = temp_ans_arr[::]
         return
@@ -16,6 +22,7 @@ def dfs(depth, pfsvc, temp_ans_arr):
     if pfsvc[4] > ans:
         # 끝까지 가지 않더라도 이미 비용이 초과
         return
+
     # 넣는다.
     for i in range(5):
         pfsvc[i] += foods[depth][i]
@@ -33,23 +40,24 @@ def dfs(depth, pfsvc, temp_ans_arr):
 
 def is_healthy(arr):
     for i in range(4):
-        if arr[i] < ms[i]:
+        if arr[i] < minimums[i]:
             return False
     return True
 
 
 N = int(input())
-ms = list(map(int, input().split()))
+minimums = list(map(int, input().split()))
 foods = [list(map(int, input().split())) for _ in range(N)]
 # 0, 1, 2, 3, cost
 
 
-ans = 500*15+1
-ans_arr = [N+2]
+ans = int(1e9)
+ans_arr = []
+# 초기값인 상태로 라인 A에 들어가지 않으니 큰 값 안 넣어도 됨
 dfs(0, [0, 0, 0, 0, 0], [])
 
 
-if ans == 500*15+1:
+if ans == int(1e9):
     print(-1)
 else:
     print(ans)
