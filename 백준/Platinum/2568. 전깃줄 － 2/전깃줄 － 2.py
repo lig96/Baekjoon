2c97c8d7259e42d0f5dd46e4ca50aa48a438c726
name_lig96 <126560547+lig96@users.noreply.github.com>
2023-12-19 02:14:36 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 284 ms, Memory: 53392 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
@@ -0,0 +1,54 @@
+# N이 클 때 길이와 수열 모두 출력하는 LIS
+
+from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+def make_dp():
+    dp = []
+    lengths = []
+
+    for i in [0]:
+        dp.append(arr[i])
+        lengths.append(len(dp))
+    for i in range(1, N):
+        if dp[-1] < arr[i]:
+            dp.append(arr[i])
+            lengths.append(len(dp))
+        else:
+            ind = bisect_left(dp, arr[i])
+            dp[ind] = arr[i]
+            lengths.append(ind+1)
+
+    return dp, lengths
+
+
+def do_print():
+    ans = set()
+    target_length = len(dp)
+
+    for i, v in list(enumerate(lengths))[::-1]:
+        if v == target_length:
+            ans.add(left[i])
+            target_length -= 1
+
+    print(str(N-len(ans))+'\n')
+    for v in left:
+        if v not in ans:
+            print(str(v)+'\n')
+
+    return
+
+
+N = int(input())
+arr = sorted([list(map(int, input().split())) for _ in range(N)])
+left = [x[0] for x in arr]
+arr = [x[1] for x in arr]
+
+
+dp, lengths = make_dp()
+
+
+do_print()
