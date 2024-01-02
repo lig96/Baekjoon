6a9e5ca3f7ec76475d9f66fd8d7b2cde7a78f9da
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-01-02 15:46:28 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 117056 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -2,13 +2,12 @@ N = int(input())
 MOD = 15746
 
 
-dp = [0 for _ in range(N+3)]
-dp[0] = 0
+dp = [0 for _ in range(N+2)]
+dp[0] = 1
 dp[1] = 1
-dp[2] = 2
 
 
-for i in range(3, N+1):
+for i in range(2, N+1):
     dp[i] = (dp[i-1] + dp[i-2]) % MOD
 
 
