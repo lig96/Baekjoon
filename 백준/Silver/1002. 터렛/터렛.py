1f096e435d7badfe84bc06f878524d715ca99d94
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-01-02 16:26:26 +0900

    Update 터렛.py

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -2,12 +2,6 @@ import sys
 input = sys.stdin.readline
 
 
-def get_dist(dot1, dot2):
-    x1, y1 = dot1
-    x2, y2 = dot2
-    return ((x1-x2)**2 + (y1-y2)**2) ** (1/2)
-
-
 T = int(input())
 for _ in range(T):
     x1, y1, r1, x2, y2, r2 = map(int, input().split())
@@ -15,17 +9,25 @@ for _ in range(T):
     if (x1, y1, r1) == (x2, y2, r2):
         print(-1)
         continue
-    dist = get_dist((x1, y1), (x2, y2))
+
+    dist = ((x1-x2)**2 + (y1-y2)**2) ** (1/2)
     sum_r = r1+r2
-    if dist > sum_r:
-        print(0)
-    elif dist == sum_r:
+    diff_r = abs(r1-r2)
+
+    if dist == sum_r:
+        # 외접
         print(1)
+    elif dist > sum_r:
+        # 외접보다 멂
+        print(0)
     elif dist < sum_r:
-        diff_r = abs(r1-r2)
-        if dist > diff_r:
-            print(2)
-        elif dist == diff_r:
+        # 외접보다 가까움
+        if dist == diff_r:
+            # 내접
             print(1)
+        elif dist > diff_r:
+            # 내접보다 멂
+            print(2)
         elif dist < diff_r:
+            # 내접보다 가까움
             print(0)
