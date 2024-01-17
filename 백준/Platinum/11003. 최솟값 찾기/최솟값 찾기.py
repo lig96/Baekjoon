469a9290c9acfa936a3b6a3dc63f9065107a5909
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-01-17 21:03:26 +0900

    Update 최솟값 찾기.py

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,4 +1,23 @@
-# 전역 변수 실험
+# 방법 1.
+# heap
+# (중요한 건 아니지만
+# 힙의 길이는 L이면 충분하지만
+# 큰 값이 힙 속에 남아있어서 L 초과일 수도 있음)
+# 푸쉬-nlogn, 팝-nlogn, 출력-n
+# O(nlogn)
+
+# 방법 2.
+# deque
+# 강단조증가가 되도록 deque를 구성하면
+# qu[0]은 언제나 최솟값임
+# 슬라이딩윈도우 속에서
+# 오른쪽이 오래 살아남으니 오른쪽이 작다면
+# 왼쪽의 작은 것을 남길 필요가 없음
+# (중요한 건 아니지만 길이는 L 이하
+# 무조건 조건에 따라 걸러내긴 해야 함)
+# 어팬드-n, 팝-n, 출력-n
+# O(n)
+
 
 from collections import deque
 from heapq import heappush, heappop
@@ -7,7 +26,7 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 
-def sol():
+def sol_deque(A_arr, N, L):
     ans = []
 
     qu = deque()
@@ -19,10 +38,27 @@ def sol():
             qu.append((A, i))
 
         # 조건 확인
-        while qu and not (i-L+1 <= qu[0][1]):
+        # 이게 매번 되기 때문에 while 안 해도 됨
+        # append도 무조건 되니 qu도 안 해도 됨
+        if not (i-L+1 <= qu[0][1]):
             qu.popleft()
 
         ans.append(qu[0][0])
+    return ans
+
+
+def sol_heap(A_arr, N, L):
+    ans = []
+
+    heap = []
+    for i, A in enumerate(A_arr):
+        heappush(heap, (A, i))
+        while True:
+            temp_A, temp_i = heappop(heap)
+            if i-L+1 <= temp_i:
+                ans.append(temp_A)
+                heappush(heap, (temp_A, temp_i))
+                break
     return ans
 
 
@@ -30,7 +66,7 @@ N, L = map(int, input().split())
 A_arr = list(map(int, input().split()))
 
 
-ans = sol()
+ans = sol_deque(A_arr, N, L)
 
 
 print(' '.join(map(str, ans)))
