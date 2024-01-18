0ea46a8f7147f0c445594df7d9db44f23a1f5e6d
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-01-19 01:44:49 +0900

    Update 건공펀치 등차수열.py

--- 백준/Silver/30825. 건공펀치 등차수열/건공펀치 등차수열.py
+++ 백준/Silver/30825. 건공펀치 등차수열/건공펀치 등차수열.py
@@ -3,14 +3,17 @@ A_arr = list(map(int, input().split()))
 
 
 first_element = A_arr[0]
+
+
 ans = 0
 for i, A in enumerate(A_arr):
-    diff = (first_element + K*i) - A
+    diff = (first_element + K*i) - (A)
     if diff >= 0:
-        ans += diff
+        ans += diff  # 자기 자신
     else:
-        ans += (-diff * i)
-        first_element += -diff
+        # 자기 자신은 0
+        ans += (abs(diff) * i)  # A0~Ai-1, i개
+        first_element += abs(diff)  # 초항
 
 
 print(ans)
