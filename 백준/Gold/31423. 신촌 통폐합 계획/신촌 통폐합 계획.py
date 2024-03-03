266596d67d458625debaf47f4725c07bc4da54af
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-03-03 09:48:00 +0900

    Update 신촌 통폐합 계획.py

--- 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.py
+++ 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.py
@@ -1,3 +1,14 @@
+# 풀이 1
+# 동순위 처리 방식이 약간 특이한 dfs
+# 깊이 우선이되, 자식 노드가 왼쪽부터 pop되게끔
+# 뒤집어서 append 해줘야 한다.
+# 재귀 방식으로 dfs를 구현하면
+# 메모리 초과 혹은 시간 초과가 나는 듯하다.
+
+# 풀이 2
+# 연결 리스트
+
+
 import sys
 input = sys.stdin.readline
 print = sys.stdout.write
@@ -17,5 +28,5 @@ for _ in range(N-1):
 
 
 for _ in range(N):
-    print(schools[i])  # 맨 마지막의 i
+    print(schools[i])  # 초기값은 맨 마지막의 i
     i = nxt[i]
