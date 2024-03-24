79150f1e106718d9b513aacc5060789107da5c00
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-03-24 17:18:02 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 964 ms, Memory: 49852 KB -BaekjoonHub

---
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,45 @@
+# 바로 아래 pypy랑 동일하고 python으로 제출 언어만 변경
+
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+def make_OPN():
+    sieve = [True for _ in range(max_n+1)]
+    # sieve[i] = 숫자 i가 소수인지 여부
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(2, int(max_n**0.5)+1):
+        if sieve[i]:
+            for j in range(i*i, max_n+1, i):
+                sieve[j] = False
+
+    return [i for i, v in enumerate(sieve[3:], start=3) if v]
+
+
+def sol(n, arr, set_):
+    for i in range(len(arr)):
+        a, b = arr[i], n-arr[i]
+        if b in set_:
+            return f"{n} = {a} + {b}"
+    return "Goldbach's conjecture is wrong."
+
+
+max_T = 100_000
+max_n = 1_000_000
+
+
+arr_OPN = make_OPN()
+set_OPN = set(arr_OPN)
+# odd_prime_numbers
+
+
+for _ in range(max_T):
+    n = int(input())
+    if n == 0:
+        break
+
+    ans = sol(n, arr_OPN, set_OPN)
+
+    print(ans+'\n')
