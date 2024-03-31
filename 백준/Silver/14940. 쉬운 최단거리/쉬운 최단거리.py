58a5487b3fc743460e17645292032f9b027d3045
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-03-31 21:29:36 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 464 ms, Memory: 40004 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,54 +1,52 @@
+# 똑같은데 코드만 정리
+
+
 from collections import deque
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
 
 
-n, m = map(int, input().split())
-mat = []
-loc = False
-for row in range(n):
-    temp = list(map(int, input().split()))
-    mat.append(temp)
+def bfs(graph, startr, startc, visited):
+    qu = deque()
+    qu.append((startr, startc))
+    visited[startr][startc] = 0
 
-    if (not loc) and (2 in temp):
-        loc = (row, temp.index(2))
+    while qu:
+        r, c = qu.popleft()
+        for i in range(4):
+            newr, newc = r+dr[i], c+dc[i]
+            if 0 <= newr < R and 0 <= newc < C and visited[newr][newc] == -1:
+                if graph[newr][newc] == 1:
+                    # 갈 수 있는 땅
+                    qu.append((newr, newc))
+                    visited[newr][newc] = visited[r][c]+1
+    return
 
 
-visit = [['N' for _ in range(m)] for _ in range(n)]
+R, C = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(R)]
+for r in range(R):
+    for c in range(C):
+        if graph[r][c] == 2:
+            startr, startc = r, c
+
+
+visited = [[-1 for _ in range(C)] for _ in range(R)]
 dr = [0, 0, 1, -1]
 dc = [1, -1, 0, 0]
 
 
-def bfs(graph, v, visit):
-    row, col = v
-    qu = deque()
-    qu.append((row, col, 0))
-    visit[row][col] = 0
-
-    while qu:
-        nowr, nowc, nowlevel = qu.popleft()
-
-        for i in range(4):
-            newr, newc = nowr+dr[i], nowc+dc[i]
-
-            if (0 <= newr < n) and (0 <= newc < m):
-                # mat 안이라면
-                if (visit[newr][newc] == 'N'):
-                    # visit 아닌 상태
-                    if mat[newr][newc] == 1:
-                        # 갈 수 있는 땅
-                        visit[newr][newc] = nowlevel+1
-                        qu.append((newr, newc, nowlevel+1))
-                    elif mat[newr][newc] == 0:
-                        # 갈 수 없는 땅
-                        visit[newr][newc] = 0
+bfs(graph, startr, startc, visited)
 
 
-bfs(mat, loc, visit)
-
-for row in range(len(visit)):
-    for col in range(len(visit[0])):
-        if (visit[row][col] == 'N') and (mat[row][col] == 1):
-            visit[row][col] = -1
-        elif (visit[row][col] == 'N') and (mat[row][col] == 0):
-            visit[row][col] = 0
-for row in visit:
-    print(*row)
+# 각 지점에서 목표지점까지의 거리를 출력한다.
+# 원래 갈 수 없는 땅인 위치는 0을 출력하고,
+# 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치는 -1을 출력한다.
+for r in range(R):
+    for c in range(C):
+        if graph[r][c] == 0:
+            print(str(0)+' ')
+        else:
+            print(str(visited[r][c])+' ')
+    print('\n')
