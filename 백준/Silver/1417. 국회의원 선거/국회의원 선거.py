8f2fdffcbbe3d3ccdbb9f7911ae7307822764018
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-05-22 13:22:27 +0900

    Update 국회의원 선거.py

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,22 +1,26 @@
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
 
 
 N = int(input())
 dasom = int(input())
-votes = [-int(input()) for _ in range(N-1)] + [-(-float('inf'))]
+votes = [int(input()) for _ in range(N-1)]
 
 
-heapify(votes)
+heap = []
+for vote in votes:
+    heappush(heap, -vote)
+heappush(heap, -(-float('inf')))
+# 최대힙이라서 마이너스 붙여줘야 함.
+# IndexError 방지하기 위해 매우 작은 값 하나 넣어줌.
 
 
 cnt = 0
-while dasom <= -votes[0]:
-    temp = heappop(votes)
+while dasom <= -heap[0]:
+    temp = heappop(heap)
     temp = (-1 * temp) - 1
-    heappush(votes, -temp)
-
+    heappush(heap, -temp)
     cnt += 1
     dasom += 1
 
