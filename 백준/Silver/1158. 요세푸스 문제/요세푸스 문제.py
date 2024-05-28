96c33644ef05c6efd5fbcd52095cfe1935b1b568
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-05-28 16:28:32 +0900

    Update 요세푸스 문제.py

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,7 +1,7 @@
 from collections import deque
 import sys
-# input = sys.stdin.readline
-# print = sys.stdout.write
+input = sys.stdin.readline
+print = sys.stdout.write
 
 
 N, K = map(int, input().split())
@@ -12,7 +12,8 @@ qu = deque(str(i) for i in range(1, N+1))
 
 temp = []
 for _ in range(N):
-    qu.rotate(-((K-1) % N))
+    rot = (K-1) % N
+    qu.rotate(-rot)
     temp.append(qu.popleft())
 
 
