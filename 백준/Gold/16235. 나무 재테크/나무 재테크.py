8b43d26ae97b6a6ccb9cbac1c6ef732f337b6f62
name_lig96 <126560547+lig96@users.noreply.github.com>
2024-09-15 08:57:25 +0900

    Update 나무 재테크.py

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,9 +1,13 @@
-# 봄여름가울겨울 함수 4개
-# 1차원 배열에 (r, c, z). PyPy3 - 1172 ms
-# N*N 2차원 배열에 z. PyPy3 - 752 ms
+# do_sp, do_sum, do_fall, do_win 함수 4개
+# 무한한 1차원 배열에 (r, c, z). PyPy3 - 1172ms
+# N*N 2차원 배열에 (z). PyPy3 - 752ms
 #
-# 상수 커팅, 함수 합치기
-# N*N 2차원 배열에 z. PyPy3 - 지금
+# do_sp, do_sum_fall_win 함수 2개
+# 무한한 1차원 배열에 (r, c, z). PyPy3 - 안해봄
+# N*N 2차원 배열에 z. PyPy3 - 564ms
+#
+# 위는 모두 deque 사용 기준이고
+# 딕셔너리로 나무의 크기별 개수 관리하고 정렬은 key만 정렬하면 200ms까지 가능
 
 
 from collections import deque
@@ -11,7 +15,7 @@ import sys
 input = sys.stdin.readline
 
 
-def do_spring_summer():
+def do_spring():
     for r, c in range_rc:
         now_trees = trees[r][c]
         for _ in range(len(now_trees)):
@@ -24,8 +28,9 @@ def do_spring_summer():
     return
 
 
-def do_fall():
+def do_summer_fall_winter():
     for r, c in range_rc:
+        # fall
         now_trees = trees[r][c]
         for z in now_trees:
             # 자기 자신에게는 나무가 안 생기기 때문에 임시 복사 없이 해도 됨
@@ -36,11 +41,7 @@ def do_fall():
                 if not (0 <= newr < N and 0 <= newc < N):
                     continue
                 trees[newr][newc].appendleft(1)  # appendleft, 오름차순 유지
-    return
-
-
-def do_summer_winter():
-    for r, c in range_rc:
+        # summer, winter
         land[r][c] += A_arr[r][c] + dead_trees[r][c]
         dead_trees[r][c] = 0
     return
@@ -54,19 +55,18 @@ for _ in range(M):
     x, y, z = map(int, input().split())
     trees[x-1][y-1].append(z)  # zero-indexing
 # 첫 입력은 동일 위치에 나무가 생기지 않기 때문에
-# 위치마다 다른 데큐면 정렬할 필요 없음
-dead_trees = [[0 for _ in range(N)] for _ in range(N)]  # 시작값 5
+# 각 좌표마다 따로 데큐를 만들었으면 정렬할 필요 없음
+dead_trees = [[0 for _ in range(N)] for _ in range(N)]
 
 
 dr, dc = [-1, -1, -1, 0, 0, 1, 1, 1], [-1, 0, 1, -1, 1, -1, 0, 1]
 range_rc = [(r, c) for r in range(N) for c in range(N)]
-# 제너레이터는 일회용이라 안 됨
+# 제너레이터 익스프레션은 일회용이라 안 되고 리스트, 튜플, 제너레이터 함수 써야 함
 
 
 for _ in range(K):
-    do_spring_summer()
-    do_fall()
-    do_summer_winter()
+    do_spring()
+    do_summer_fall_winter()
 
 
 ans = 0
