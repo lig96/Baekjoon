7adac263901cc97b90882c08737c1d2179d66b5c
name_lig96 <126560547+lig96@users.noreply.github.com>
2025-01-12 23:54:08 +0900

    Update 직사각형.py

--- 백준/Silver/2527. 직사각형/직사각형.py
+++ 백준/Silver/2527. 직사각형/직사각형.py
@@ -11,31 +11,34 @@ def sol(line):
     if (b_x1 < a_x2 and b_x2 > a_x1) and (b_y1 < a_y2 and b_y2 > a_y1):
         ret.append('a')
 
-    if (b_x1 == a_x2 or b_x2 == a_x1) ^ (b_y1 == a_y2 or b_y2 == a_y1):
-        # xor
+    if (((b_x1 == a_x2 or b_x2 == a_x1) and (b_y1 <= a_y2 and b_y2 >= a_y1))
+                ^
+                (((b_x1 <= a_x2 and b_x2 >= a_x1) and (b_y1 == a_y2 or b_y2 == a_y1)))
+            ):
+        # x좌표가 선분에 닿는 것과 함께 y좌표가 겹치는지 확인해야 하고, vice versa임.
+        # y좌표의 겹침을 확인하지 않으면 아예 겹치지 않더라도 이 경우에 해당하게 됨.
+        # 선분에 닿는 걸 확인해야 하니 'a' 조건과 달리 등호가 붙어야 함.
+        # or 아니고 xor해야 함.
         ret.append('b')
 
-    if (b_x1 == a_x2 or b_x2 == a_x1) and (b_y1 == a_y2 or b_y2 == a_y1):
+    if (((b_x1 == a_x2 or b_x2 == a_x1) and (True))
+                and
+                (((True) and (b_y1 == a_y2 or b_y2 == a_y1)))
+            ):
+        # if (b_x1 == a_x2 or b_x2 == a_x1) and (b_y1 == a_y2 or b_y2 == a_y1):
         ret.append('c')
 
     if (b_x1 > a_x2 or b_x2 < a_x1) or (b_y1 > a_y2 or b_y2 < a_y1):
         ret.append('d')
 
-    if 'd' in ret and 'b' in ret:
-        ret.remove('b')
     return ret
 
 
-for line in sys.stdin.readlines():
-    line = line.rstrip()
-    line = list(map(int, line.split()))
+for _ in range(4):
+    line = list(map(int, sys.stdin.readline().split()))
 
     ans = sol(line)
 
-    if len(ans) == 0:
-        print(line)
-        exit(2)
-    if len(ans) >= 2:
-        print(line, ans)
-        raise RecursionError
+    if len(ans) != 1:
+        raise Exception
     sys.stdout.write(str(ans[0])+'\n')
