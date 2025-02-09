a269720356a930f4001000b24fd88d704360158d
name_lig96 <126560547+lig96@users.noreply.github.com>
2025-02-09 16:51:16 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 76 ms, Memory: 35532 KB -BaekjoonHub

--- 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.py
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.py
@@ -1,67 +1,52 @@
+# "임의의 노드의 왼쪽 부트리에 있는 노드들은 해당 노드보다 왼쪽의
+# 열에 위치하고, 오른쪽 부트리에 있는 노드들은 해당 노드보다
+# 오른쪽의 열에 위치한다."는 글자 그대로 중위 순회를 의미한다.
+# 따라서 중위 순회를 통해 방문된 순서가 곧 열 좌표이다.
+#
+# 루트 노드는 부모 노드로서는 나타났으면서 자식 노드로서는 안
+# 나타난 노드이다. 이를 알아내기 위해 그래프 순회를 할 필요는 없다.
+
+
+from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
 
-def rec(i):
-    if width[i] is not None:
-        return sum(width[i].values())
-    temp = {}
-    temp['self'] = 1
-    temp['left'] = 0 if nodes[i]['left'] == -1 else rec(nodes[i]['left'])
-    temp['right'] = 0 if nodes[i]['right'] == -1 else rec(nodes[i]['right'])
-    width[i] = temp
-    return sum(width[i].values())
+def inorder(x, lv):
+    if x == -1:
+        return
 
+    global turn
 
-def dfs(startx):
-    stack = [(startx, 1)]
-    while stack:
-        now, lv = stack.pop()
-        for lr_char in ['left', 'right']:
-            child = nodes[now][lr_char]
-            if child == -1:
-                continue
-            stack.append((child, lv+1))
-            loc[child] = (loc[now] +
-                          (-1 if lr_char == 'left' else 1) *
-                          (sum(width[child].values()) - width[child][lr_char])
-                          )
-            depth[lv+1] = [min(depth[lv+1][0], loc[child]),
-                           max(depth[lv+1][1], loc[child])]
+    inorder(nodes[x]['left'], lv+1)
+    depth[lv] = [min(depth[lv][0], turn),
+                 max(depth[lv][1], turn)]
+    turn += 1
+    inorder(nodes[x]['right'], lv+1)
     return
 
 
 N = int(input())
-nodes = dict()
-for _ in range(N):
-    parent, left, right = map(int, input().split())
-    nodes[parent] = {'left': left, 'right': right}
+nodes = {par: {'left': l, 'right': r} for par, l, r
+         in (map(int, input().split()) for _ in range(N))}
 
 
-# 루트 = summation 1..N - 자식 노드
+# 루트 = 부모 노드 - 자식 노드 = summation 1..N - 자식 노드
 root = (N * (N+1) // 2) - \
     sum(x for dic in nodes.values() for x in dic.values() if x != -1)
-
-
-# width[i] = 노드 i를 루트로 하는 트리 전체의 너비
-width = [None for _ in range(N+1)]
-rec(root)  # width 배열 채우기
 # depth[d] = [d 깊이의 노드 중 가장 왼쪽 좌표, d 깊이의 노드 중 가장 오른쪽 좌표]
-depth = [[float('inf'), -float('inf')] for _ in range(N+1)]
-depth[0] = None  # 없는 인덱스
-depth[1] = [0, 0]  # 깊이 1은 [0, 0]으로 고정
-# loc[i] = 노드 i의 위치
-loc = [None for _ in range(N+1)]
-loc[root] = 0  # 루트 노드는 0으로 고정
-dfs(root)  # depth와 loc 배열 채우기
+depth = defaultdict(lambda: [float('inf'), -float('inf')])
+# turn = 특정한 노드가 순회된 순서
+turn = 1
+
+
+inorder(x=root, lv=1)
 
 
 ans = {'ind': float('inf'), 'width': -float('inf')}
-for i, v in enumerate(depth):
-    if v == None:
-        continue
+for k, v in sorted(depth.items(), key=lambda x: x[0]):
     temp = v[1]-v[0]+1
     if temp > ans['width']:
-        ans['ind'] = i
+        ans['ind'] = k
         ans['width'] = temp
 print(ans['ind'], ans['width'])
