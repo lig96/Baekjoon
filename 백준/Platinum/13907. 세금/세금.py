ad17bc6d44c904556b9e37cd35e2f7456ab92d9d
name_lig96 <126560547+lig96@users.noreply.github.com>
2025-09-07 12:11:23 +0900

    Update 세금.py

--- 백준/Platinum/13907. 세금/세금.py
+++ 백준/Platinum/13907. 세금/세금.py
@@ -1,7 +1,17 @@
-'''
-시간 확인용
-https://www.acmicpc.net/source/88378309
-'''
+# 강단조함수의 형태를 구현해야 한다.
+# 이 조건에 맞지 않아
+# 원소가 dist에 추가되지 않는다면 그만큼 그 원소의 child도
+# 연산을 하지 않아 소요 시간이 줄어든다.
+# 이때 이를 위해 다소 비효율적인 O(N)을 해도 된다.
+# 8000ms 초과 -> 4000ms
+#
+# 정답을 출력할 때 마지막 인덱스를 조금씩 줄여나가는 것이 중요하다.
+# 어떤 세금의 인덱스 k에 대하여 정답이 되는 인덱스가 i라면
+# j > i에 대해서 이미 v_j+세금*j > v_i+세금*i인데
+# 세금은 증가함수이니 앞으로도 해당 부등식은 성립하기 때문이다.
+# 이를 통해 O(KN) 반복문의 시간 복잡도를 줄일 수 있다.
+# 4000ms -> 200ms
+
 
 from heapq import heappush, heappop
 import sys
@@ -9,8 +19,6 @@ input = sys.stdin.readline
 
 
 def dijk(S):
-    INF = float('inf')
-
     dist = [[INF for _ in range(N)] for _ in range(N+1)]
     heap = []
 
@@ -23,18 +31,18 @@ def dijk(S):
             continue
         if (length+1) >= N:
             continue
-        if any(v < now_d for v in dist[now][:length]):
-            # v < now_d가 하나라도 있다면 해당 temp는 무의미하다.
-            # 사용한 도로의 수도 많은데 비용도 많다는 뜻이기 때문이다.
-            # 위치는 시간과 전혀 상관이 없었다.
-            continue
         for child, cost in graph[now]:
             temp = now_d + cost
             temp_l = length+1
             if temp < dist[child][temp_l]:
+                if any(v <= temp for v in dist[child][:temp_l]):
+                    # v <= temp가 하나라도 있다면 해당 temp는 무의미하다.
+                    # 사용한 도로의 수도 많은데 비용도 많다는 뜻이기 때문이다.
+                    # 이로써 강단조함수를 만들 수 있다.
+                    # 이 조건문의 위치는 소요 시간과 전혀 상관이 없었다.
+                    continue
                 dist[child][temp_l] = temp
                 heappush(heap, (temp, temp_l, child))
-
     return dist
 
 
@@ -50,37 +58,29 @@ for _ in range(M):
 taxes = [int(input()) for _ in range(K)]
 
 
-# presum_taxes = [0]
-# for v in taxes:
-#     presum_taxes.append(presum_taxes[-1]+v)
-# # 세금 인상은 누적으로 적용된다.
+INF = float('inf')
+presum_taxes = [0]
+for v in taxes:
+    presum_taxes.append(presum_taxes[-1]+v)
+# 세금 인상은 누적으로 적용된다.
 
 
-# dist_S_D: list[int] = dijk(S)[D]
-# # dist_S_D[i] = v,
-# # S에서 D까지 i개의 도로를 사용했을 때 최단 거리는 v이다.
+dist_S_D: list[int] = dijk(S)[D]
+# dist_S_D[i] = v,
+# S에서 D까지 i개의 도로를 사용했을 때 최단 거리는 v이다.
 
 
+last = len(dist_S_D)-1  # 마지막으로 가능한 인덱스
+for tax in presum_taxes:
+    ans = INF  # min의 항등원
+    for i in range(last+1):  # 마지막으로 가능한 인덱스를 포함
+        v = dist_S_D[i]
+        temp = v+i*tax
+        if temp < ans:
+            ans = temp
+            last = i  # 마지막으로 가능한 인덱스
+    print(ans)
+# # 하단과 기본적으로 동일하다.
 # for tax in presum_taxes:
 #     temp = (v+i*tax for i, v in enumerate(dist_S_D))
 #     print(min(temp))
-
-'''
-시간 확인용
-https://www.acmicpc.net/source/88378309
-'''
-distance = dijk(S)[D]
-
-
-taxes = [0] + taxes
-pay = 0
-last_idx = N-1
-for tax in taxes:
-    pay += tax
-    ans = float('inf')
-    for idx in range(1, last_idx+1):
-        cost = distance[idx] + idx*pay
-        if ans > cost:
-            ans = cost
-            last_idx = idx
-    print(ans)
